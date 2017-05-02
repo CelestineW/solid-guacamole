81cdb637ad8c77bed5c5ae5e23c3734e0ac1888b
Celestine Wong <cwong2@umbc.edu>
2017-05-01 21:46:58 -0400

    More UI components, including some intents

---
+++ app/src/main/java/ru/buepl/mobile/application/CreateAccountActivity.java
@@ -0,0 +1,13 @@
+package ru.buepl.mobile.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account3);
+    }
+}
--- app/src/main/java/ru/buepl/mobile/application/ProgramChoiceActivity.java
+++ app/src/main/java/ru/buepl/mobile/application/ProgramChoiceActivity.java
@@ -1,13 +1,69 @@
 package ru.buepl.mobile.application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProgramChoiceActivity extends AppCompatActivity {
+public class ProgramChoiceActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button programButton, appButton, webButton, galleryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_choice);
+
+        programButton = (Button) findViewById(R.id.b_academicProgs);
+        appButton = (Button) findViewById(R.id.b_app);
+        webButton = (Button) findViewById(R.id.b_mainWeb);
+        galleryButton = (Button) findViewById(R.id.b_gallery);
+
+        programButton.setOnClickListener(this);
+        appButton.setOnClickListener(this);
+        webButton.setOnClickListener(this);
+        galleryButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.b_academicProgs:
+
+                /*
+                Intent academicPrograms = new Intent(this, AcademicActivity.class);
+                startActivity(academicPrograms);
+                */
+
+                break;
+            case R.id.b_app:
+
+                /*
+                Intent applicationIntent = new Intent(this, ApplicationActivity.class);
+                startActivity(applicationIntent);
+                 */
+
+                break;
+            case R.id.b_mainWeb:
+                /* -- is this needed, to open up in chrome/safari?
+                Intent mainWebIntent  = new Intent(this, ApplicationActivity.class);
+                startActivity(applicationIntent);
+                 */
+                break;
+
+            case R.id.b_gallery:
+                 /*
+                Intent galleryIntent = new Intent(this, GalleryActivity.class);
+                startActivity(galleryIntent);
+                 */
+                break;
+
+        }
+
     }
 }
