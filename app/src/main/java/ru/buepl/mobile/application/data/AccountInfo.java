ffef0292c63cbe342b7a3fb17a1d9329bb90912e
ashero <ol1@umbc.edu>
2017-05-04 20:45:35 -0400

    Add validation interface and methods
    Add `Validatable` interface and `Validation` utility class.
    
    Add some string resources.
    
    Implement validation for a couple of classes.

--- app/src/main/java/ru/buepl/mobile/application/data/AccountInfo.java
+++ app/src/main/java/ru/buepl/mobile/application/data/AccountInfo.java
@@ -1,11 +1,21 @@
 package ru.buepl.mobile.application.data;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Data;
+import ru.buepl.mobile.application.R;
+import ru.buepl.mobile.application.data.validation.Validatable;
+import ru.buepl.mobile.application.data.validation.ValidationError;
+
+import static ru.buepl.mobile.application.data.validation.Validation.*;
 
 @Data
 @Builder(builderClassName = "Builder")
-public final class AccountInfo {
+public final class AccountInfo implements Validatable {
     private String firstName = "";
     private String middleName = "";
     private String lastName = "";
@@ -22,4 +32,18 @@ public final class AccountInfo {
      * For serialization purposes
      */
     private AccountInfo() {}
+
+    @NonNull
+    @Override
+    public List<ValidationError> validate(@NonNull Context context) {
+        return aggregateErrors(
+                nonEmptyString(firstName, R.string.first_name, context),
+                nonEmptyString(lastName, R.string.last_name, context),
+                nonEmptyString(address, R.string.address, context),
+                nonEmptyString(city, R.string.city, context),
+                nonEmptyString(zipCode, R.string.zip_code, context),
+                nonEmptyString(country, R.string.country, context),
+                nonEmptyString(email, R.string.email, context)
+        );
+    }
 }
