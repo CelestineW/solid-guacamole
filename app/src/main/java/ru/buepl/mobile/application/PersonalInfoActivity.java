b86c8ce2d4f9abde3ff5f70601b7dab9b7775762
MinJei <swat1128@hotmail.com>
2017-05-09 23:23:13 -0400

    EditText

--- app/src/main/java/ru/buepl/mobile/application/PersonalInfoActivity.java
+++ app/src/main/java/ru/buepl/mobile/application/PersonalInfoActivity.java
@@ -5,19 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import ru.buepl.mobile.application.data.shared.Identification;
 
 public class PersonalInfoActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button nextButton;
+
+    EditText editTextHomePhoneNumber;
+    EditText editTextCellPhoneNumber;
+    EditText editTextPassportNumber;
+    EditText editTextIssueDate;
+    EditText editTextIssuedBy;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
 
-        nextButton = (Button) findViewById(R.id.button8);
+        editTextHomePhoneNumber = (EditText) findViewById(R.id.editText7);
+        editTextCellPhoneNumber = (EditText) findViewById(R.id.editText9);
+        editTextPassportNumber = (EditText) findViewById(R.id.editText12);
+        editTextIssueDate = (EditText) findViewById(R.id.editText14);
+        editTextIssuedBy = (EditText) findViewById(R.id.editText23);
 
+        nextButton = (Button) findViewById(R.id.button8);
         nextButton.setOnClickListener(this);
     }
 
@@ -27,6 +40,13 @@ public class PersonalInfoActivity extends AppCompatActivity implements View.OnCl
         switch(v.getId()){
 
             case R.id.button8:
+
+                String homePhoneNumberTxt = editTextHomePhoneNumber.getText().toString();
+                String cellPhoneNumberTxt = editTextCellPhoneNumber.getText().toString();
+                String passportNumberTxt = editTextPassportNumber.getText().toString();
+                String issueDateTxt = editTextIssueDate.getText().toString();
+                String issuedByTxt = editTextIssuedBy.getText().toString();
+
                 Intent identificationIntent = new Intent(this, IdentificationActivity.class);
                 startActivity(identificationIntent);
 
--- app/src/main/java/ru/buepl/mobile/application/SignInActivity.java
+++ app/src/main/java/ru/buepl/mobile/application/SignInActivity.java
@@ -5,15 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button signIn;
 
+    EditText editTextUsername;
+    EditText editTextPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        editTextUsername = (EditText) findViewById(R.id.editText);
+        editTextPassword = (EditText) findViewById(R.id.editText2);
 
         signIn = (Button) findViewById(R.id.button4);
         signIn.setOnClickListener(this);
@@ -25,6 +32,9 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         switch (v.getId()){
 
             case R.id.button4:
+
+                String usernameTxt = editTextUsername.getText().toString();
+                String passwordTxt = editTextPassword.getText().toString();
 
                 Intent programChoice = new Intent(this, MainMenuActivity.class);
                 startActivity(programChoice);
