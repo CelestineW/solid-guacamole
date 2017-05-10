43c184ca12312ea4e319b66ea20da6d80cf347a6
MinJei <swat1128@hotmail.com>
2017-05-10 02:44:36 -0400

    EditText

--- app/src/main/java/ru/buepl/mobile/application/ProgramSelectionActivity.java
+++ app/src/main/java/ru/buepl/mobile/application/ProgramSelectionActivity.java
@@ -1,13 +1,52 @@
 package ru.buepl.mobile.application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProgramSelectionActivity extends AppCompatActivity {
+import ru.buepl.mobile.application.data.HigherEducationApplication;
+
+public class ProgramSelectionActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button higherEdu, professionalDev;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_selection);
+
+        higherEdu = (Button)findViewById(R.id.higherEducation);
+        professionalDev = (Button) findViewById(R.id.professional);
+
+        higherEdu.setOnClickListener(this);
+        professionalDev.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    /*  higherEducation and professionalDevelopment application page
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.higherEducation:
+                Intent higherEducationApplicationIntent = new Intent(this, .class);
+                startActivity(higherEducationApplicationIntent);
+                break;
+
+            case R.id.professional:
+                Intent professionalDevelopmentIntent = new Intent(this, .class);
+                startActivity(professionalDevelopmentIntent);
+
+                break;
+        }
+    }
+
+    */
+
 }
